a5dd40df7a3dc7eba90397342c60246cdff4590b
isksss <104404522+isksss@users.noreply.github.com>
2024-04-15 13:55:18 +0000

    185-a

---
+++ atcoder/abc/185/a.rs
@@ -0,0 +1,15 @@
+use proconio::input;
+
+fn main(){
+    input!{
+        a:[i64; 4],
+    }
+    let mut min: i64 = 100;
+    for i in &a{
+        if i < &min{
+            min = *i;
+        }
+    }
+
+    println!("{}", min);
+}
