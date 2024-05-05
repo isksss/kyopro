9399fb9629e7a50642d9778df113f8dd9d432a31
isksss <104404522+isksss@users.noreply.github.com>
2024-05-05 05:32:01 +0000

    352-a-rust

--- atcoder/abc352/src/bin/a.rs
+++ atcoder/abc352/src/bin/a.rs
@@ -4,16 +4,20 @@ use proconio::input;
 fn main() {
     // ===== input =====
     input!{
+        _n: usize,
         x: usize,
         y: usize,
         z: usize,
     }
 
     // ===== answer =====
-    let mut ans: usize = 0;
+    let mut ans: &str = "No";
 
     // ===== solve =====
-    
+    if (x<y && x<z && z<y) || (x>y && y<z && z<x){
+        ans = "Yes";
+    }
+
     // ===== output =====
     println!("{}", ans);
 }
