581185dfdbd773b02a170e79bb35eb5cf788e21b
isksss <104404522+isksss@users.noreply.github.com>
2024-05-05 05:20:00 +0000

    352 init

---
+++ atcoder/abc352/src/bin/e.rs
@@ -0,0 +1,20 @@
+#![allow(unused)]
+use proconio::input;
+
+fn main() {
+    // ===== input =====
+    input!{
+        x: usize,
+        y: usize,
+        z: usize,
+    }
+
+    // ===== answer =====
+    let mut ans: usize = 0;
+
+    // ===== solve =====
+    
+    // ===== output =====
+    println!("{}", ans);
+}
+
